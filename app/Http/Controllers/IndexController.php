bc9544a1bd7e6b01291370769b3ff9a0eb2df022
macarons <514870412@qq.com>
2018-09-25 14:35:28 +0800

    区块浏览器首页

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,6 +9,7 @@ use App\Model\Transaction;
 use App\Model\TxOut;
 use App\Service\APIService;
 use App\Service\SyncService;
+use App\Services\RpcService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
@@ -22,62 +23,29 @@ class IndexController extends Controller
      */
     public function index()
     {
-        $data['currentPage'] = 'index';
-        return view("index.index",$data);
-    }
+        $rpcService = new RpcService();
+        $lastBlock = $rpcService->lastBlockHeightNumber();
 
-    /**
-     * 搜索
-     * @param Request $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function search(Request $request)
-    {
-        $keyword = $request->input('keyword');
-
-        //判断是否为数字，如果为数字，优先查询区块
-        if(is_numeric($keyword))
+        $lastBlock = base_convert($lastBlock,16,10);
+        $blockString = "[";
+        for($i=0;$i<10;$i++)
         {
-            $res = Block::where('height',$keyword)->first();
-            if(isset($res) && $res->hash_id)
-            {
-                $url = "/block/detail?hash=".$res->hash_id;
-                return redirect($url);
-
-            }else{
-                //todo 跳转404页面
-                return back();
-            }
-        }else{
-            //如果不为数字，优先查询交易
-            $res = Transaction::where('tx_hash',$keyword)->count();
-            if($res > 0)
-            {
-                $url = "/tx/".$keyword;
-                return redirect($url);
-            }else{
-                //如果不是交易，则查询是否为区块
-                $res = Block::where('hash_id',$keyword)->count();
-                if($res > 0)
-                {
-                    $url = "/block/detail?hash=".$keyword;
-                    return redirect($url);
-                }else{
-                    //如果不为区块，则查询是否为地址
-                    $res = Account::where('address',$keyword)->count();
-                    if($res > 0)
-                    {
-                        $url = "/address/".$keyword;
-                        return redirect($url);
-                    }else{
-                        //todo 跳转404页面
-                        return back();
-                    }
-                }
-
-
-            }
+            $blockString = $blockString . '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x'.base_convert($lastBlock--,10,16).'",true],"id":1},';
         }
+        $blockString = rtrim($blockString,",");
+        $blockString = $blockString . "]";
+        $block = $rpcService->getBlockByNumber($blockString);
+        $blockList = array();
+        foreach ($block as $key => $item)
+        {
+            $blockList[$key] = $item['result'];
+            $blockList[$key]['height'] = base_convert($blockList[$key]['number'],16,10);
+            $blockList[$key]['created_at'] = date("Y-m-d H:i:s",base_convert($blockList[$key]['timestamp'],16,10)+28800);
+            $blockList[$key]['tx_count'] = count($blockList[$key]['transactions']);
+        }
+
+        $data['block'] = $blockList;
+        return view("index.index",$data);
     }
 
 }
--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -18,64 +18,24 @@
                         </thead>
                         <!---->
                         <tbody class="">
-                            {{--@foreach($list as $item)--}}
-                                {{--<tr class="">--}}
-                                    {{--<td aria-colindex="1" class="">--}}
-                                        {{--<a href="/block/detail?hash={{$item->hash_id}}" class="text3">{{$item->height}}</a>--}}
-                                    {{--<td aria-colindex="2" class="">--}}
-                                        {{--<span class="block-time" style="text-align: center;">{{$item->created_at}}</span>--}}
-                                    {{--</td>--}}
-                                    {{--<td aria-colindex="3" class="">{{$item->tx_count}}</td>--}}
-                                    {{--<td aria-colindex="4" class="pc-hash">--}}
-                                        {{--<a href="/block/detail?hash={{$item->hash_id}}" class="text3 vtext-monospace">{{$item->hash_id}}</a>--}}
-                                    {{--</td>--}}
-                                    {{--<td aria-colindex="4" class="web-hash">--}}
-                                        {{--<a href="/block/detail?hash={{$item->hash_id}}" class="text3 vtext-monospace">{{mb_substr($item->hash_id,0,18,'utf-8')}}</a>--}}
-                                    {{--</td>--}}
-                                {{--</tr>--}}
-                            {{--@endforeach--}}
+                            @foreach($block as $item)
+                                <tr class="">
+                                    <td aria-colindex="1" class="">
+                                        <a href="/block/detail?hash={{$item['hash']}}" class="text3">{{$item['height']}}</a>
+                                    <td aria-colindex="2" class="">
+                                        <span class="block-time" style="text-align: center;">{{$item['created_at']}}</span>
+                                    </td>
+                                    <td aria-colindex="3" class="">{{$item['tx_count']}}</td>
+                                    <td aria-colindex="4" class="pc-hash">
+                                        <a href="/block/detail?hash={{$item['hash']}}" class="text3 vtext-monospace">{{$item['hash']}}</a>
+                                    </td>
+                                    <td aria-colindex="4" class="web-hash">
+                                        <a href="/block/detail?hash={{$item['hash']}}" class="text3 vtext-monospace">{{mb_substr($item['hash'],0,18,'utf-8')}}</a>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
-                </div>
-            </div>
-            <div class="panel-network row">
-                <div class="panel col-12 col-lg-12">
-                    <div class="clearfix panel-header">
-                        <span class="float-left">全网数据</span></div>
-                    <div class="row panel-container">
-                        <div class="inner-panel col-12 col-lg-12 collapsed-padding">
-                            <ul class="vsection vshadow">
-                                <li class="item">
-                                    <span class="text2 strong">全网算力</span>
-                                    {{--<span class="text2">{{$total_power}}万</span>--}}
-                                </li>
-                                <li class="item">
-                                    <span class="text2 strong">总积分数</span>
-                                    {{--<span class="text2">{{$amount}}万 QKI</span>--}}
-                                </li>
-                                <li class="item">
-                                    <span class="text2 strong">有积分的地址数</span>
-                                    {{--<span class="text2">{{$address_available}}</span>--}}
-                                </li>
-                                <li class="item">
-                                    <span class="text2 strong">总节点数</span>
-                                    {{--<span class="text2">{{$node_num}}</span>--}}
-                                </li>
-                                <li class="item">
-                                    <span class="text2 strong">总用户数</span>
-                                    {{--<span class="text2">{{$user_num}}</span>--}}
-                                </li>
-                                <li class="item">
-                                    <span class="text2 strong">总冻结数</span>
-                                    {{--<span class="text2">{{$freeze_amount}}万 QKI</span>--}}
-                                </li>
-                                <li class="item">
-                                    <span class="text2 strong">24小时交易笔数</span>
-                                    {{--<span class="text2">{{$transaction_num}}</span>--}}
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
