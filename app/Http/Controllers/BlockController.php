e597c361594974683fb36521c6e730a5ffb14c0b
dafei <476469085@qq.com>
2018-09-25 11:39:16 +0800

    区块列表

---
+++ app/Http/Controllers/BlockController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class BlockController extends Controller
+{
+    public function index()
+    {
+
+        return view("block.index");
+    }
+
+
+
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@
 */
 
 Route::get('/', 'IndexController@index');
+Route::get('/block', 'BlockController@index');
