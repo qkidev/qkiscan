10919c6ab6fcff23c8c07d388aed1e8469980e46
yyxo <550503811@qq.com>
2018-11-02 14:24:16 +0800

    区块详情接口

--- app/Http/Controllers/Api/BlockController.php
+++ app/Http/Controllers/Api/BlockController.php
@@ -32,4 +32,43 @@ class BlockController extends Controller
 
         return response()->json(['code' => 0, 'message' => 'OK', 'data' => $blockList]);
     }
+
+    /**
+     * 获取区块详情
+     * @param Request $request
+     * @param RpcService $rpcService
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function blockDetail(Request $request, RpcService $rpcService)
+    {
+        $hash = $request->input('hash');
+        if (!$hash) {
+            return response()->json(['code' => 500, 'message' => '缺少必要参数', 'data' => '']);
+        }
+        $blockInfo = $rpcService->getBlockByHash($hash);
+        if(!isset($blockInfo['result']))
+        {
+            return response()->json(['code' => 500, 'message' => '请输入正确的区块HASH', 'data' => '']);
+        }
+        $blockInfo = $blockInfo['result'];
+        $data = [];
+        if ($blockInfo)
+        {
+            $data['hash'] = $hash;
+            $data['height'] = base_convert($blockInfo['number'],16,10);
+            $data['created_at'] = date("Y-m-d H:i:s",base_convert($blockInfo['timestamp'],16,10)+28800);
+            $data['tx_count'] = count($blockInfo['transactions']);
+            $data['size'] = bcdiv(base_convert($blockInfo['size'],16,10),1000,3);
+            $data['miner'] = $blockInfo['miner'];
+            $data['difficulty'] = base_convert($blockInfo['difficulty'],16,10);
+            $data['transactions'] = [];
+            foreach($blockInfo['transactions'] as $k => $v)
+            {
+                $data['transactions'][$k]['hash'] = $v['hash'];
+                $data['transactions'][$k]['amount'] = bcdiv(base_convert($v['value'],16,10) ,gmp_pow(10,18),18);
+            }
+        }
+
+        return response()->json(['code' => 0, 'message' => 'OK', 'data' => $data]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['namespace' => 'Api'], function () {
-    Route::get('get-token-balance', 'BalanceController@getTokenBalance');
-    Route::get('get-token-tx', 'TransactionsController@getTokenTx');
-    Route::get('get-qki-tx', 'TransactionsController@getTransactions');
-    Route::get('get-qki-balance', 'BalanceController@getQkiBalance');
-    Route::get('get-block', 'BlockController@getBlock');
+    Route::get('get-token-balance', 'BalanceController@getTokenBalance');//获取通证余额
+    Route::get('get-qki-balance', 'BalanceController@getQkiBalance');//获取QKI余额
+    Route::get('get-token-tx', 'TransactionsController@getTokenTx');//获取通证交易记录
+    Route::get('get-qki-tx', 'TransactionsController@getTransactions');//获取QKI交易记录
+    Route::get('get-block', 'BlockController@getBlock');//获取区块列表
+    Route::get('block', 'BlockController@blockDetail');//获取区块列表
 });
