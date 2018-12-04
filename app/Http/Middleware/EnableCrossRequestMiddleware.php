c7f3a2c367ee2360ff1c50e50c9d79f8a1d91f5c
yyxo <550503811@qq.com>
2018-12-04 18:56:18 +0800

    跨域

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -18,10 +18,6 @@ class EnableCrossRequestMiddleware
         $response = $next($request);
         $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
         $response->header('Access-Control-Allow-Origin',$origin);
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
-        $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-        $response->header('Access-Control-Allow-Credentials', 'true');
         return $response;
     }
 }
