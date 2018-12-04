2edd3bd1863b113b8c6beda1104b63afb9ba4bfe
yyxo <550503811@qq.com>
2018-12-04 19:08:35 +0800

    跨域修改

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -18,6 +18,7 @@ class EnableCrossRequestMiddleware
         $response = $next($request);
         $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
         $response->header('Access-Control-Allow-Origin',$origin);
+        $response->header('Access-Control-Allow-Headers', '*');
         $response->header('Cache-control','public');
         $response->header('Allow','GET,POST,OPTIONS');
         return $response;
