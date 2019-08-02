ec4f2d88bcbf3a153d88a1aee8cf5ddcfb4ed931
周长城 <1911574296@qq.com>
2019-08-02 11:53:59 +0800

    去除8小时

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -56,9 +56,10 @@ class IndexController extends Controller
             $data['max_height'] = $blockList[0]['height']+1;
         }
         $data['transactions_num'] = Transactions::where('tx_status', 1)->count();
-        $start = time()-24*60*60-28800;
-        $end = time()-28800;
-        $data['hour_24_num'] = Transactions::where('tx_status', 1)->whereRaw("unix_timestamp(created_at)<$end")->whereRaw("unix_timestamp(created_at)>$start")->count();
+        $start = time()-24*60*60;
+        $end = time();
+        $data['hour_24_num'] = Transactions::where('tx_status', 1)->whereRaw("unix_timestamp(updated_at)<$end")->
+        whereRaw("unix_timestamp(updated_at)>$start")->count();
         $data['address_num'] = Balances::where('name', 'qki')->where('amount', '>', 0)->count();
         $data['block'] = $blockList;
         $data['currentPage'] = "index";
