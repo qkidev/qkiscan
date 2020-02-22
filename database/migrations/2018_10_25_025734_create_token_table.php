d8af9ebae08cf4a07bc1db26ae58302d76066288
chenjia404 <chenjiablog@qq.com>
2020-02-22 12:52:00 +0800

    修正字段长度

--- database/migrations/2018_10_25_025734_create_token_table.php
+++ database/migrations/2018_10_25_025734_create_token_table.php
@@ -15,8 +15,8 @@ class CreateTokenTable extends Migration
     {
         Schema::create('token', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('token_name',20)->comment('token名');
-            $table->string('token_symbol')->comment('token标识');
+            $table->string('token_name',128)->comment('token名');
+            $table->string('token_symbol',128)->comment('token标识');
             $table->string('contract_address', 42)->unique()->comment('合约地址');
             $table->timestamps();
         });
