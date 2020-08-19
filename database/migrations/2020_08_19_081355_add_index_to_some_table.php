14963c5c838aa21fe1dc2c4a747e32f7a3a6170c
wzf <727711866@qq.com>
2020-08-19 16:19:57 +0800

    修复慢查询-给字段添加索引

---
+++ database/migrations/2020_08_19_081355_add_index_to_some_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexToSomeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('token_tx', function (Blueprint $table) {
+            //添加联合索引
+            $table->index(['token_id', 'tx_status']);
+        });
+        Schema::table('transactions', function (Blueprint $table) {
+            //添加索引
+            $table->index('updated_at');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('token_tx', function (Blueprint $table) {
+            //
+            $table->dropIndex(['token_id', 'tx_status']);
+        });
+        Schema::table('transactions', function (Blueprint $table) {
+            //
+            $table->dropIndex('updated_at');
+        });
+    }
+}
